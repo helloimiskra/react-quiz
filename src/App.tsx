8f2bc309dbbc0ebf5d238f4648b0ed0ad9e7dde7
helloimiskra <helloimiskra@gmail.com>
2020-12-08 14:22:40 +0900

    sets up api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import QuestionCard from './components/QuestionCard'
+import React, { useState } from 'react';
+import QuestionCard from './components/QuestionCard';
+import { fetchQuizQuestions } from './API';
+import { Difficulty } from './API'
 
-function App() {
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver]  = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY ))
   
   const startTrivia = async () => {
 
-  }
+  };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
 
-  }
+  };
 
   const nextQuestion = () => {
 
 
-  }
+  };
   
   
   return (
@@ -26,7 +38,15 @@ function App() {
 
       <p className="score">Score</p>
       <p>Loading Questions...</p>
-      <QuestionCard/>
+      {/* <QuestionCard
+        questionNr={number+1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswers={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+        
+        /> */}
       <button className ="next" onClick={nextQuestion}>
         Next Question
       </button>
